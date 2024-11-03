f97c974f7089eb910aadb2099c3a1285cef9797c
Mason Jennings <masonj@csu.fullerton.edu>
2024-11-03 01:29:15 -0700

    first value readable!

--- sort.c
+++ sort.c
@@ -3,9 +3,11 @@
 // Author email: masonj@csu.fullerton.edu
 #include <stdio.h>
 
-int sort(int len, double *array) {
+int sort(int len, unsigned long int *array) {
 
     printf("\nHello from sort.c!!!!\n");
+    printf("there are %d elements (random numbers) in the array", len);
+    printf("The first random number is 0x%016lx", *array);
     //make sure program can read array by displaying elements of the array first
     //then, make the sorting algorithm (bubble sort?)
 
